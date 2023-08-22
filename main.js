bae344466a11137591847d0ddf2b56a0a863ae0d
Golberd12rus <96867093+Golberd12rus@users.noreply.github.com>
2023-08-22 16:01:04 +0300

    Add files via upload

--- main.js
+++ main.js
@@ -1,1 +1,26 @@
 $(".news-container").bxSlider();
+
+$('.form').submit(e => {
+e.preventDefault();
+
+  $.fancybox.open({
+    src: "#modal",
+    type: "inline"
+  })
+});
+
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
